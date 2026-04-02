a180505d1dcad1de450445f0f758af9f7665650e
Sergiu <szatmari.sergiu@gmail.com>
2026-04-02 22:22:12 +0300

    min

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import { router } from "./routes/index.ts";
 import { registerRoutes } from "./routes/index.ts";
 import { config } from "./config.ts";
 
+// Firstly, register all routes
+// after initializing the router
 registerRoutes(router);
 
-// Start the server and listen for requests
+// Then, start the server & listen for requests
 Deno.serve({ port: config.port }, (request: Request) => {
   return router.handle(request);
 });
